722e023b021a64eae7809514e8e7519d1649b7d9
Nate Schultz <ngs32@drexel.edu>
2022-07-31 14:10:36 -0400

    move url request to new func

--- Main.js
+++ Main.js
@@ -1,31 +1,42 @@
+const { resolve } = require('path');
 const puppeteer = require('puppeteer');
 
-function run () {
+const base = 'https://en.wikipedia.org/';
+
+function get_data(link, extract) {
     return new Promise(async (resolve, reject) => {
         try {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
-            await page.goto("https://en.wikipedia.org/wiki/Lists_of_brands");
-            let urls = await page.evaluate(() => {
-                let results = [];
-                let items = document.querySelectorAll('a');
-                items.forEach((item) => {
-                    results.push({
-                        url:  item.getAttribute('href'),
-                        title:  item.getAttribute('title'),
-                    });
-                });
-                const regex = new RegExp('List of.*brands', 'g');
-                results = results.filter((row) => row['url'] !== null && row['title'] !== null);
-                results = results.filter((row) => row['title'].match(regex));
-                return results;
-            })
+            await page.goto(link);
+            let extraction = await page.evaluate(extract)
             browser.close();
-            return resolve(urls);
+            return resolve(extraction);
         } catch (e) {
             return reject(e);
         }
     })
 }
 
-run().then(console.log).catch(console.error);
+function get_brand_types() {
+    extract = () => {
+        let ret = [];
+        let items = document.querySelectorAll('a');
+        items.forEach((item) => {
+            ret.push({
+                url:  item.getAttribute('href'),
+                title:  item.getAttribute('title'),
+            });
+        });
+        const regex = new RegExp('List of.*brands', 'g');
+        ret = ret.filter((row) => row['url'] !== null && row['title'] !== null);
+        ret = ret.filter((row) => row['title'].match(regex));
+        return ret;
+    }
+
+    get_data(base+"/wiki/Lists_of_brands", extract).then( (ret) => {
+        console.log(ret);
+    }).catch(console.error)
+}
+
+get_brand_types()
