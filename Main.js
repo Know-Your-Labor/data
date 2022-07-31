ac7d831681ed5a2e34695b8809c472e2650f7662
Nate Schultz <ngs32@drexel.edu>
2022-07-31 15:24:21 -0400

    misc

--- Main.js
+++ Main.js
@@ -23,27 +23,52 @@ function get_brand_types() {
         let ret = [];
         let items = document.querySelectorAll('a');
         items.forEach((item) => {
-            ret.push({
-                url:  item.getAttribute('href'),
-                title:  item.getAttribute('title'),
-            });
+            const title = item.getAttribute('title');
+            const url = item.getAttribute('href')
+            if(url && title) {
+                ret.push({
+                    url: url.trim(),
+                    title: title.trim(),
+                    description: title.replace('List of ', '').trim()
+                });
+            }
         });
-        // const regex = new RegExp('List of.*brands', 'g');
+        const regex = new RegExp('List of.*brand.*', 'g');
         ret = ret.filter((row) => row['url'] !== null && row['title'] !== null);
-        // ret = ret.filter((row) => row['title'].match(regex));
+        ret = ret.filter((row) => row['title'].match(regex));
         return ret;
     }
 
     get_data(base+"/wiki/Lists_of_brands", extract).then( (ret) => {
         console.log("Found " + ret.length + " brand types");
         ret.forEach( (brand_type) => {
-            get_brands(brand_type);
+            setTimeout(function(){
+                get_brands(brand_type);
+            }, 1000);
         })
     }).catch(console.error);
 }
 
-function get_brands() {
+function get_brands(brand_type) {
+    extract = () => {
+        let ret = [];
+        let table_of_contents = document.querySelectorAll("#toc > ul > * a")
+        // filter out "See also" and "References"
+        // get all the links from that section
+        document.querySelector("#Lists_of_brands").parentNode.nextElementSibling.nextElementSibling.querySelectorAll("a")
+    }
 
+    get_data(base+brand_type['url'], extract).then( (ret) => {
+        console.log("Found " + ret.length + " " + brand_type['description']);
+        ret.forEach( (brand) => {
+            console.log(brand);
+        })
+    }).catch(console.error)
 }
 
-get_brand_types()
+// get_brand_types()
+get_brands({
+    url: '/wiki/List_of_brand_name_snack_foods',
+    title: 'List of brand name snack foods',
+    description: 'brand name snack foods'
+})
